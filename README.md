8f0b394bdf05026b1f4b2636273b9a1eaf13f458
dwinuroktaviani27 <1941720239@student.polinema.ac.id>
2022-08-29 17:45:49 +0700

    Praktikum 3

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # flutter_fundamental_part1
 
 ## PRAKTIKUM 1
-![PRAKTIKUM 1](images/prak1.png)
+![PRAKTIKUM 1](images/prak1.PNG)
+## PRAKTIKUM 3
+Langkah 1
+![PRAKTIKUM 1](images/prak3l1.PNG)
+Langkah 2
+![PRAKTIKUM 1](images/prak3l2.PNG)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamental_part1/basic_widgets/image_widget.dart';
+import 'basic_widgets/text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -95,9 +97,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const MyTextWidget(),
+            const MyImageWidget(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
